16a35c486ba1db6f5ca6ef40fee95d7d3fde4bbf
Simen <lsimen89@gmail.com>
2018-04-10 18:16:18 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/no/sichqu/robocode/Sichqu.java
+++ src/no/sichqu/robocode/Sichqu.java
@@ -1,9 +1,17 @@
 package no.sichqu.robocode;
 
+<<<<<<< Updated upstream
 import robocode.AdvancedRobot;
 
 public class Sichqu extends AdvancedRobot {
     public void run(){
         //Test
+=======
+import robocode.*;
+
+public class sichqu extends AdvancedRobot {
+    public void run(){
+        
+>>>>>>> Stashed changes
     }
 }
